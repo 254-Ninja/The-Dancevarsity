76e59f2fecedf5b78eeb6fded7d62285a269b40e
254-ninja <muthomidenis62@gmail.com>
2020-10-05 22:52:12 +0300

    login update

--- app/src/main/java/com/emeq/dancevarsity/loginscreen.java
+++ app/src/main/java/com/emeq/dancevarsity/loginscreen.java
@@ -2,13 +2,33 @@ package com.emeq.dancevarsity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class loginscreen extends AppCompatActivity {
+
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginscreen);
+
+        // Locate the button in activity_main.xml
+        button = findViewById(R.id.button2);
+        // Capture button clicks
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(loginscreen.this,
+                        MainActivity.class);
+                startActivity(myIntent);
+
+            }
+        });
+
     }
 }
