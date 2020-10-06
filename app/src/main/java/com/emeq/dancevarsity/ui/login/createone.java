41307dae1967dd1c9ba61dee40dfe5a578264f79
254-ninja <muthomidenis62@gmail.com>
2020-10-06 11:35:36 +0300

    sign up

---
+++ app/src/main/java/com/emeq/dancevarsity/ui/login/createone.java
@@ -0,0 +1,16 @@
+package com.emeq.dancevarsity.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.emeq.dancevarsity.R;
+
+public class createone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_one);
+    }
+}
